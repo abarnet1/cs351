a1762f69b4e0492e76c58d394447bd0bb264d727
lee@soi2.org <lee@soi2.org@ae098f79-844e-4119-8f95-5ac3c23d9c6a>
2008-02-14 05:25:26 +0000

    Updating mailer script.
    git-svn-id: http://svn.devjavu.com/iitcs/cs351@7 ae098f79-844e-4119-8f95-5ac3c23d9c6a

--- labs/lab1/mailer.rb
+++ labs/lab1/mailer.rb
@@ -1,45 +1,66 @@
-#!/usr/bin/env ruby
+#! /usr/bin/env ruby
 
-### Set the following two variables to your own full name and 8 digit CWID ###
+### Set the following two variables to your own full name(s) and 8 digit CWID(s) ###
+### If you aren't working in a group, remove the second name/CWID                ###
 
-NAME = "John Doe"
-CWID = "12345678"
+NAME = [ "John Doe", "Jane Adams"]
+CWID = [ 12345678,   34567890   ]
 
 ##############################################################################
 
 ### Don't touch anything below this line! #################################### 
 
-require 'tempfile'
+require 'net/smtp'
+require 'rubygems'
+require 'mailfactory'
 
-DEST_EMAIL = 'lee@soi2.org'
-LAB_NAME   = 'shlab'
+DEST_EMAIL = 'lee@iit.edu'
+COURSE_ID  = 'CS 351'
+LAB_NAME   = 'clab'
 
-if !File.executable?('/usr/sbin/sendmail')
-  puts "Can't find sendmail binary"
+if ARGV.length < 1
+  puts "No attachments specified.  Submission aborted."
   exit
 end
 
-mailfile = Tempfile.new('mailfile')
-mailfile << "Subject: #{LAB_NAME} submission for #{NAME}/#{CWID}\n"
+mail = MailFactory.new
+mail.to = DEST_EMAIL
+mail.from = "cs351@host220.cns.iit.edu"
+
+if NAME.size != CWID.size
+  puts "Number of names must match the number of CWIDs!"
+  exit
+else
+  if NAME[0] =~ /John Doe/ and CWID[0] == 12345678
+    puts "Change your name(s)/cwid(s) in the #{$0} script before submitting!"
+    exit
+  end
+  pretty_names = NAME[0]
+  mail_text = "Lab submission for:\n\t#{NAME[0]} / #{CWID[0]}\n"
+  for i in 1...NAME.length
+    pretty_names += " and #{NAME[i]}"
+    mail_text += "\t#{NAME[i]} / #{CWID[i]}\n"
+  end
+  mail.subject = "#{COURSE_ID} #{LAB_NAME} submission for #{pretty_names}"
+  mail.text = mail_text
+end
 
 ARGV.each do |to_send| 
-  if to_send.nil? or !File.exists?(to_send)
-    puts "Can't find file: #{to_send}"
+  if !(File.exists?(to_send) and File.readable?(to_send))
+    puts "Can't read or locate file: #{to_send}"
     exit
   else
-    puts "Sending #{to_send}, last modification time #{File.mtime(to_send)}..."
-    mailfile << "*" * 30 + " " +  to_send + " " + "*" * 30 + "\n\n"
-    mailfile << File.read(to_send) + "\n\n"
-    mailfile << "*" * 80 + "\n\n"
+    puts "Sending #{File.basename(to_send)}, last modified #{File.mtime(to_send)}..."
+    mail.attach(to_send)
   end
 end
 
-mailfile.flush
-
-if (system("/usr/sbin/sendmail #{DEST_EMAIL} < #{mailfile.path}"))
-  puts "Handin successful for #{NAME}/#{CWID}."
-else
-  puts "Handin unsuccessful!"
+begin
+  Net::SMTP.start('localhost', 25) do |smtp|
+    smtp.send_message(mail.to_s(), mail.from.first, mail.to.first)
+  end
+  puts "Submission successful for #{pretty_names}"
+rescue Exception => e
+  puts "Error sending mail: #{e}"
+  exit
 end
-
-mailfile.close
