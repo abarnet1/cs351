a18bb326314640ce565634e573889a593322b770
lee@soi2.org <lee@soi2.org@ae098f79-844e-4119-8f95-5ac3c23d9c6a>
2008-02-16 04:56:01 +0000

    Adding interactive mailer script
    git-svn-id: http://svn.devjavu.com/iitcs/cs351@11 ae098f79-844e-4119-8f95-5ac3c23d9c6a

--- labs/lab1/mailer.rb
+++ labs/lab1/mailer.rb
@@ -1,66 +1,95 @@
 #! /usr/bin/env ruby
 
-### Set the following two variables to your own full name(s) and 8 digit CWID(s) ###
-### If you aren't working in a group, remove the second name/CWID                ###
-
-NAME = [ "John Doe", "Jane Adams"]
-CWID = [ 12345678,   34567890   ]
-
-##############################################################################
-
-### Don't touch anything below this line! #################################### 
-
 require 'net/smtp'
 require 'rubygems'
 require 'mailfactory'
 
-DEST_EMAIL = 'lee@iit.edu'
+SMTP_HOST  = 'smtp.rcn.com'
+SMTP_PORT  = 25
+DEST_EMAIL = 'lee@soi2.org'
+FROM_EMAIL = 'host220.cns.iit.edu'
 COURSE_ID  = 'CS 351'
 LAB_NAME   = 'clab'
 
-if ARGV.length < 1
-  puts "No attachments specified.  Submission aborted."
-  exit
+class Student 
+  attr_accessor :name, :cwid
+  def to_s
+    @name
+  end
 end
 
-mail = MailFactory.new
-mail.to = DEST_EMAIL
-mail.from = "cs351@host220.cns.iit.edu"
+def read_team
+  team = []
+  loop do
+    member = Student.new
+    print "Submitter name: "
+    member.name = $stdin.gets.chomp
+    print "Submitter CWID: "
+    member.cwid = $stdin.gets.chomp
+    team << member
+  
+    print "More team members? (y/N): "
+    break unless $stdin.gets.downcase =~ /y/
+  end
+  return team
+end
 
-if NAME.size != CWID.size
-  puts "Number of names must match the number of CWIDs!"
-  exit
-else
-  if NAME[0] =~ /John Doe/ and CWID[0] == 12345678
-    puts "Change your name(s)/cwid(s) in the #{$0} script before submitting!"
+if $0 == __FILE__
+  if ARGV.length < 1
+    puts "No attachments specified.  Submission aborted."
     exit
   end
-  pretty_names = NAME[0]
-  mail_text = "Lab submission for:\n\t#{NAME[0]} / #{CWID[0]}\n"
-  for i in 1...NAME.length
-    pretty_names += " and #{NAME[i]}"
-    mail_text += "\t#{NAME[i]} / #{CWID[i]}\n"
+
+  mail = MailFactory.new
+  mail.to = DEST_EMAIL
+  mail.from = FROM_EMAIL
+
+  team = []
+  loop do
+    puts "\nEnter team information:"
+    team = read_team
+
+    if team.size > 2
+      puts "\nWarning: too many team members!"
+    end
+
+    puts "\nSubmitting for:"
+    team.each do |member|
+      puts "\t#{member.name} / #{member.cwid}"
+    end
+    
+    print "Continue? (Y/n): "
+    
+    break unless $stdin.gets.downcase =~ /n/
   end
+
+  pretty_names = team.inject {|anded, n| "#{anded} and #{n}"}
   mail.subject = "#{COURSE_ID} #{LAB_NAME} submission for #{pretty_names}"
+  
+  mail_text = "Lab submission for:\n\t#{team[0].name} / #{team[0].cwid}\n"
+  for i in 1...team.length
+    mail_text += "\t#{team[i].name} / #{team[i].cwid}\n"
+  end
   mail.text = mail_text
-end
 
-ARGV.each do |to_send| 
-  if !(File.exists?(to_send) and File.readable?(to_send))
-    puts "Can't read or locate file: #{to_send}"
+  puts
+  ARGV.each do |to_send| 
+    if !(File.exists?(to_send) and File.readable?(to_send))
+      puts "Can't read or locate file: #{to_send}"
+      exit
+    else
+      puts "Sending #{File.basename(to_send)}, last modified #{File.mtime(to_send)}..."
+      mail.attach(to_send)
+    end
+  end
+
+  begin
+    Net::SMTP.start(SMTP_HOST, SMTP_PORT) do |smtp|
+      smtp.send_message(mail.to_s(), mail.from.first, mail.to.first)
+    end
+    puts "Submission successful for #{pretty_names}"
+  rescue Exception => e
+    puts "Error sending mail: #{e}"
     exit
-  else
-    puts "Sending #{File.basename(to_send)}, last modified #{File.mtime(to_send)}..."
-    mail.attach(to_send)
   end
-end
-
-begin
-  Net::SMTP.start('localhost', 25) do |smtp|
-    smtp.send_message(mail.to_s(), mail.from.first, mail.to.first)
-  end
-  puts "Submission successful for #{pretty_names}"
-rescue Exception => e
-  puts "Error sending mail: #{e}"
-  exit
 end
